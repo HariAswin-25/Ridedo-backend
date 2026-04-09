476c4fec86158e0298e274f8c919a5040122b35e
HariAswin-25-4 <hari.senthilkumar@fssa.frfeshworks.com>
2026-04-09 20:07:53 +0530

    fixed driver

--- schemas/driver_booking.py
+++ schemas/driver_booking.py
@@ -12,7 +12,19 @@ class DriverBookingCreate(BaseModel):
     booking_time: str
     duration_hours: int
     contact_number: str
+class DriverBookingUpdate(BaseModel):
+    user_id: Optional[int]
+    driver_id: Optional[int]
+    pickup_location: Optional[str]
+    drop_location: Optional[str]
+    booking_date: Optional[str]
+    booking_time: Optional[str]
+    duration_hours: Optional[int]
+    contact_number: Optional[str]
+    status: Optional[str]
 
+    class Config:
+        orm_mode = True 
 
 # RESPONSE
 class DriverBookingOut(DriverBookingCreate):
