b864546857b78c3f6cc7b48487faa03ab57c9308
HariAswin-25-4 <hari.senthilkumar@fssa.frfeshworks.com>
2026-04-15 14:44:28 +0530

    Changed CORS in main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://ridedo-frontend-jb0i7czf7-hariaswin2007-5936s-projects.vercel.app"
+    "https://ridedo-frontend-jb0i7czf7-hariaswin2007-5936s-projects.vercel.app",
     "https://ridedo-backend-4o1g.vercel.app"  # later use this
 ]
 
